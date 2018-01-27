02c79431e5a0ef25fd59a361c92b142600f06fcd
Andrei Lapshin <alapshin@fastmail.com>
2018-01-27 09:51:41 +0300

    Add script to configure master node usage

---
+++ scripts/src/main/groovy/com/alapshin/jenkins/init/03configure-master-node.groovy
@@ -0,0 +1,13 @@
+package main.groovy.com.alapshin.jenkins.init
+
+import hudson.model.Node
+import jenkins.model.Jenkins
+import org.yaml.snakeyaml.Yaml
+
+
+config = new File(Jenkins.instance.getRootDir(), "config.yml").withInputStream {
+    return new Yaml().load(it)
+}
+
+Jenkins.instance.mode = Node.Mode."${config.jenkins.mode}"
+Jenkins.instance.save()
